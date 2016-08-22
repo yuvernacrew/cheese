6f3f044143350d7a5e74d593870752fccce78921
加藤　優奈 <yuvernacrew@gmail.com>
2016-08-23 03:03:16 +0900

    no message

--- js/cheese2.js
+++ js/cheese2.js
@@ -3,11 +3,12 @@ $(function(){
 	var p2 = $(".article2").offset().top-400;
 	var p3 = $(".article3").offset().top-300;
 
+	setTimeout(function(){
+		$('.article1').animate({opacity:'1'},600); 
+	},200);
+
 	$(window).scroll(function(){
 		var w = $(window).scrollTop();
-		if(w>p){
-			$('.article1').animate({opacity:'1'},600); 
-		}
 		if(w>p2){
 			$('.article2').animate({opacity:'1'},600); 
 		}
